202d7e1c7ae02f0051a18bfc2b65e2eb61459d71
wkapga <wkapga@rcm.at>
2017-09-28 14:11:55 +0200

    import file

---
+++ import.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Visualisierung JPM EMU"
+#' author: "wkapga"
+#' date: " 2017"
+#' ---
+
+library(readr)
+library(dplyr)
+library(lubridate)
+library(magrittr)
+library(treemap)
+library(tidyr)
+library(stringr)
+
+#' ## Import GBI
