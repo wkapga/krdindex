7204c50d52e0196425c384ed2aa55e08c729c959
wkapga <wkapga@rcm.at>
2017-09-29 10:43:23 +0200

    import newest indexfile

--- import.R
+++ import.R
@@ -4,12 +4,19 @@
 #' date: " 2017"
 #' ---
 
-library(readr)
-library(dplyr)
-library(lubridate)
-library(magrittr)
-library(treemap)
-library(tidyr)
+library(tidyverse)
 library(stringr)
 
+
 #' ## Import GBI
+#' 
+#' path
+path_indexfiles <- "/home/wkapga/thinclient_drives/Z:/jpm/Kursversorgung/Jpm/Done"
+#' get file list 
+list_indexfiles <- list.files(path = path_indexfiles,pattern="GBROAD") 
+
+#' extract dates a la 20140505 and find position of highest (newest) date
+  list_indexfiles %>% str_extract_all("\\d{8}") %>%  unlist() %>% which.max -> i
+#' read this file
+indexdata <- file.path(path_indexfiles,list_indexfiles[i]) %>%  read_csv(skip=1)
+  
